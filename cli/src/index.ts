056e554eaa712f06c7ec9d00158cf83faae69180
Hob Nguyen <hoanghuynguyen0403@gmail.com>
2026-05-20 09:10:59 +0700

    chore(release): cli-v2.4.2

--- cli/src/index.ts
+++ cli/src/index.ts
@@ -20,7 +20,7 @@ program
   .description(
     'A CLI to manage and sync AI agent skills for Cursor, Claude, Copilot, Windsurf, and more.',
   )
-  .version('2.4.1');
+  .version('2.4.2');
 
 program
   .command('init')
